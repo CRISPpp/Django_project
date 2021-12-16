9e7570bb9292f7c8ae5aaa2bfc0693aa4220a4a1
crisp <2715554797@qq.com>
2021-12-16 12:47:58 +0800

    finish canvas

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -53,13 +53,94 @@ class AcGameMenu {
     }
 }
 
+let AC_GAME_OBJECTS = [];
+class AcGameObject{
+    constructor() {
+        AC_GAME_OBJECTS.push(this);
+        this.has_called_start = false;//标记start有没有被执行
+        this.timedelta = 0;//距离上一帧的时间ms， 有的浏览器调用requestAnimationFrame没有一秒60次
+    }
+
+    start(){// 第一帧
+        
+    }
+
+    update(){//每帧
+
+    }
+    
+    on_destroy(){//operation before destroy
+
+    }
+
+    destroy(){
+        this.on_destroy();
+
+        for(let i = 0; i < AC_GAME_OBJECTS.length; i ++){
+            if(AC_GAME_OBJECTS[i] === this){
+                AC_GAME_OBJECTS.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+
+let last_timestamp;
+let AC_GAME_ANIMATION = function(timestamp){
+    for(let i = 0; i < AC_GAME_OBJECTS.length; i ++){
+        let obj = AC_GAME_OBJECTS[i];
+        if(!obj.has_called_start){
+            obj.start();
+            obj.has_called_start = true;
+        }
+        else{
+            obj.timedelta = timestamp - last_timestamp;
+            obj.update();
+        }
+    }
+    last_timestamp = timestamp;
+    requestAnimationFrame(AC_GAME_ANIMATION);//递归执行
+}
+
+requestAnimationFrame(AC_GAME_ANIMATION); //每秒执行60次
+class GameMap extends AcGameObject{
+    constructor(playground){
+        console.log("game_map");
+        super();
+        this.playground = playground;
+        this.$canvas = $(`<canvas></canvas>`);//js提供的画布渲染工具
+        this.ctx = this.$canvas[0].getContext('2d');
+        this.ctx.canvas.width = this.playground.width;
+        this.ctx.canvas.height = this.playground.height;
+        this.playground.$playground.append(this.$canvas);
+    }
+
+
+    start(){
+
+    }
+
+
+    update(){
+        this.render();
+    }
+
+    render(){
+        this.ctx.fillStyle = "rgba(0,0,0)";
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
+
+}
 class AcGamePlayground{
     constructor(root) {
         this.root = root;
-        this.$playground = $(`<div>游戏界面</div>`);
-        this.hide();
+        this.$playground = $(`<div class="ac-game-playground"></div>`);
+        // this.hide();
         this.root.$ac_game.append(this.$playground);
-        
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this); 
 
         this.start();
     }
@@ -76,11 +157,11 @@ class AcGamePlayground{
         this.$playground.hide();
     }
 } 
-class AcGame {
+export class AcGame {
     constructor(id){
         this.id = id;
         this.$ac_game = $('#' + id);
-        this.menu = new AcGameMenu(this);
+        // this.menu = new AcGameMenu(this);
         this.playground = new AcGamePlayground(this);
         this.start();
     }
---
+++ game/static/js/src/playground/ac_game_object/zbase.js
@@ -0,0 +1,51 @@
+let AC_GAME_OBJECTS = [];
+class AcGameObject{
+    constructor() {
+        AC_GAME_OBJECTS.push(this);
+        this.has_called_start = false;//标记start有没有被执行
+        this.timedelta = 0;//距离上一帧的时间ms， 有的浏览器调用requestAnimationFrame没有一秒60次
+    }
+
+    start(){// 第一帧
+        
+    }
+
+    update(){//每帧
+
+    }
+    
+    on_destroy(){//operation before destroy
+
+    }
+
+    destroy(){
+        this.on_destroy();
+
+        for(let i = 0; i < AC_GAME_OBJECTS.length; i ++){
+            if(AC_GAME_OBJECTS[i] === this){
+                AC_GAME_OBJECTS.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+
+let last_timestamp;
+let AC_GAME_ANIMATION = function(timestamp){
+    for(let i = 0; i < AC_GAME_OBJECTS.length; i ++){
+        let obj = AC_GAME_OBJECTS[i];
+        if(!obj.has_called_start){
+            obj.start();
+            obj.has_called_start = true;
+        }
+        else{
+            obj.timedelta = timestamp - last_timestamp;
+            obj.update();
+        }
+    }
+    last_timestamp = timestamp;
+    requestAnimationFrame(AC_GAME_ANIMATION);//递归执行
+}
+
+requestAnimationFrame(AC_GAME_ANIMATION); //每秒执行60次
