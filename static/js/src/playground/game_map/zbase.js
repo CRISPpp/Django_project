5ba22538ef19104711427cdd4a856833585bba5c
crisp <2715554797@qq.com>
2021-12-19 23:23:48 +0800

    create  app

---
+++ static/js/src/playground/game_map/zbase.js
@@ -0,0 +1,28 @@
+class GameMap extends AcGameObject{
+    constructor(playground){
+        console.log("game_map");
+        super();
+        this.playground = playground;
+        this.$canvas = $(`<canvas></canvas>`);//js提供的画布渲染工具
+        this.ctx = this.$canvas[0].getContext('2d');
+        this.ctx.canvas.width = this.playground.width;
+        this.ctx.canvas.height = this.playground.height;
+        this.playground.$playground.append(this.$canvas);
+    }
+
+
+    start(){
+
+    }
+
+
+    update(){
+        this.render();
+    }
+
+    render(){
+        this.ctx.fillStyle = "rgba(255,192,203,0.1)";
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
+
+}
