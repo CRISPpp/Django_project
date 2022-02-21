7cc0a93e901ea84e8494e5d5360046caed903b6f
crisp <2715554797@qq.com>
2022-02-21 14:02:02 +0800

    update thrift and match_system

--- game/consumers/multiplayer/index.py
+++ game/consumers/multiplayer/index.py
@@ -3,55 +3,84 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from django.conf import settings
 from django.core.cache import cache
+
+from match_system.src.match_server.match_service import Match
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift import Thrift
+
 class MultiPlayer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
 
     async def disconnect(self, close_code):
-        print('disconnect')
-        await self.channel_layer.group_discard(self.room_name, self.channel_name)
+        if(self.room_name):
+            await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
     async def create_player(self, data):
         self.room_name = None
+        self.uuid = data['uuid']
+        # Make socket
+        transport = TSocket.TSocket('127.0.0.1', 9090)
 
-        for i in range(1000):
-            name = "room-%d"%(i)
-            if not cache.has_key(name) or len(cache.get(name)) < settings.ROOM_CAPACITY:
-                self.room_name = name
-                break
-        if not self.room_name:
-            return
+        # Buffering is critical. Raw sockets are very slow
+        transport = TTransport.TBufferedTransport(transport)
 
-        if not cache.has_key(self.room_name):#没有房间，新开一个
-            cache.set(self.room_name, [], 3600) #3600表示一小时
-        for player in cache.get(self.room_name):
-            await self.send(text_data=json.dumps({
-                'event': "create_player",
-                'uuid': player['uuid'],
-                'username': player['username'],
-                'photo': player['photo'],
-            }))
+        # Wrap in a protocol
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
 
-        await self.channel_layer.group_add(self.room_name, self.channel_name)
+        # Create a client to use the protocol encoder
+        client = Match.Client(protocol)
+
+        # Connect!
+        transport.open()
+        client.add_player(1500, data['uuid'], data['username'], data['photo'], self.channel_name)
+
+        # Close!
+        transport.close()
+    
+        #下面是没通过thrift的代码
+        #self.room_name = None
+
+        #for i in range(1000):
+        #   name = "room-%d"%(i)
+        #    if not cache.has_key(name) or len(cache.get(name)) < settings.ROOM_CAPACITY:
+        #        self.room_name = name
+        #        break
+        #if not self.room_name:
+        #    return
+
+        #if not cache.has_key(self.room_name):#没有房间，新开一个
+        #   cache.set(self.room_name, [], 3600) #3600表示一小时
+        #for player in cache.get(self.room_name):
+        #    await self.send(text_data=json.dumps({
+        #        'event': "create_player",
+        #        'uuid': player['uuid'],
+        #        'username': player['username'],
+        #        'photo': player['photo'],
+        #    }))
+
+        #await self.channel_layer.group_add(self.room_name, self.channel_name)
         
-        players = cache.get(self.room_name)
-        players.append({
-            'uuid': data['uuid'],
-            'username': data['username'],
-            'photo': data['photo'],
-        })
-        cache.set(self.room_name, players, 3600)
+        #players = cache.get(self.room_name)
+        #players.append({
+        #    'uuid': data['uuid'],
+        #    'username': data['username'],
+        #    'photo': data['photo'],
+        #})
+        #cache.set(self.room_name, players, 3600)
 
-        await self.channel_layer.group_send(#群发
-            self.room_name,
-            {
-                'type': "group_send_event",
-                'event': "create_player",
-                'uuid': data['uuid'],
-                'username': data['username'],
-                'photo': data['photo'],
-            }
-        )
+        #await self.channel_layer.group_send(#群发
+        #    self.room_name,
+        #    {
+        #        'type': "group_send_event",
+        #        'event': "create_player",
+        #        'uuid': data['uuid'],
+        #        'username': data['username'],
+        #        'photo': data['photo'],
+        #    }
+        #)
 
     async def move_to(self, data):
         await self.channel_layer.group_send(
---
+++ match_system/src/main.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python3
+
+import glob
+import sys
+sys.path.insert(0, glob.glob('../../')[0])#加这个才能importdjango原项目的包
+
+from match_server.match_service import Match
+
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+from queue import Queue #一个安全队列
+from time import sleep
+from threading import Thread
+
+queue = Queue() #消息队列
+
+class Player:
+    #和之前cpp写的player一致
+    def __init__(self, score, uuid, username, photo, channel_name):
+        self.score = score
+        self.uuid = uuid
+        self.username = username
+        self.photo = photo
+        self.channel_name = channel_name
+        self.waiting_time = 0
+
+
+
+class MatchHandler:
+    def add_player(self, score, uuid, username, photo, channel_name):
+        player = Player(score, uuid, username, photo, channel_name)
+        queue.put(player)
+        return 0 #不返回0会出现internal error
+
+class Pool:
+    def __init__(self):
+        self.players = []
+    def add_player(self, player):
+        print("add player: %s %d" % (player.username, player.score))
+        self.players.append(player)
+    def check_match(self, a, b):
+        dt = abs(a.score - b.score)
+        a_limit = a.waiting_time * 50
+        b_limit = b.waiting_time * 50
+        return dt <= a_limit and dt <= b_limit
+    def match_success(self, pl):
+        print("match success: %s %s" % (ps[0], ps[1]))
+
+    def increase_waiting_time(self):
+        for player in self.players:
+            player.waiting_time += 1
+
+    def match(self):
+        while len(self.players) >= 2:
+            self.players = sorted(self.players, keys=lambda p: p.score)
+            flag = False
+            for i in range(len(self.players - 1)):
+                a = self.players[i]
+                b = self.players[i + 1]
+                if self.check_match(a, b):
+                    self.match_success([a, b])
+                    self.players = self.players[:i] + self.players[i + 2:] #去除匹配成功的
+                    flag = True
+                    break
+            if not flag:
+                break
+            
+
+        self.increase_waiting_time()
+        
+
+def get_player_from_queue():
+    try:
+        return queue.get_nowait()#没有元素会抛出异常
+    except:
+        return None
+
+def worker():
+    pool = Pool()
+    while True:
+        player = get_player_from_queue()
+        if player:
+            pool.add_player(player)
+        else:
+            pool.match()
+            sleep(1)#每一秒匹配一次
+
+
+if __name__ == '__main__':
+    handler = MatchHandler()
+    processor = Match.Processor(handler)
+    transport = TSocket.TServerSocket(host='127.0.0.1', port=9090)
+    tfactory = TTransport.TBufferedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+
+    # You could do one of these for a multithreaded server
+    #第一个server：每来一个请求开一个线程处理，并行度最高
+    server = TServer.TThreadedServer(processor, transport, tfactory, pfactory)
+    #匹配池并发处理，超过限制会阻塞，相当于第一个的限制版
+    # server = TServer.TThreadPoolServer(
+    #    processor, transport, tfactory, pfactory)
+    Thread(target=worker, daemon=True).start()#daemon=True表示杀掉主进程也关闭这个线程
+    print('Starting the server...')
+    server.serve()
+
+if __name__ == '__main__':
+    main()
